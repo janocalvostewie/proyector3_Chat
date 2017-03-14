5d7cc856d6c03384bf67bd8b556f6877feeb8541
janocalvostewie <acalvoogando@danielcastelao.org>
2017-03-14 10:46:37 +0100

    Implementado el webview con websocket

--- app/src/main/java/com/example/jano/chatbis/MainActivity.java
+++ app/src/main/java/com/example/jano/chatbis/MainActivity.java
@@ -2,14 +2,15 @@ package com.example.jano.chatbis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private WebView mWebView;
-
+    private WebView wv;
 
 
 
@@ -19,10 +20,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mWebView = (WebView) findViewById(R.id.weby);
-        WebSettings webSettings = mWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        mWebView.loadUrl("https://chatnodejs-jano22.c9users.io/");
+        WebView wv = (WebView)findViewById(R.id.weby);
+        wv.getSettings().setJavaScriptEnabled(true);
+
+        wv.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+            }
+        });
+        wv.setWebViewClient(
+                new WebViewClient(){
+                    public boolean shouldOverrideUrlLoading(WebView view, String url){
+                        view.loadUrl(url);
+                        return false;
+                    }
+                });
+        wv.loadUrl("https://chatnodejs-jano22.c9users.io/");
+        wv.addJavascriptInterface(new WebSocketFactory(wv), "WebSocketFactory");
+       // mWebView.loadUrl();
 
     }
 }
